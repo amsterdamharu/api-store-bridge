c38c724e32f1b0c88159a531bed01b6bfde2abed
Harm Meijer <harm.meijer@commercetools.com>
2021-03-20 08:53:07 +0100

    add override

--- src/data/apiStoreBridge.test.ts
+++ src/data/apiStoreBridge.test.ts
@@ -1,4 +1,6 @@
-import createBridge from "./apiStoreBridge";
+import createBridge, {
+  CREATE_SELECTOR,
+} from "./apiStoreBridge";
 import { AVAILABLE, NOT_REQUESTED } from "./result";
 test("selector returns not requested for query by id", () => {
   expect(
@@ -42,4 +44,26 @@ test("selector returns for query page", () => {
   expect(
     createBridge().createSelectResult({})(state)
   ).toEqual({ ...AVAILABLE, value: ["a", "b"] });
+});
+test("selector can override", () => {
+  const query = { id: 1 };
+  const state: any = {
+    data: {
+      1: 88,
+    },
+  };
+  const config = {
+    override: (_type: any, cb: any) => (...args: any[]) =>
+      cb(...args),
+  };
+  expect(
+    createBridge(config).createSelectResult(query)(state)
+  ).toBe(88);
+  config.override = (type) => (query: any) => (
+    state: any
+  ) =>
+    type === CREATE_SELECTOR ? [query, state, 88] : "nope";
+  expect(
+    createBridge(config).createSelectResult(query)(state)
+  ).toEqual([query, state, 88]);
 });
--- src/data/apiStoreBridge.ts
+++ src/data/apiStoreBridge.ts
@@ -14,37 +14,45 @@ interface Arg {
   getId?: (arg0: Id) => string | number;
   path?: string[];
   queryToString?: (arg0: object) => string;
+  override?: (
+    arg0: string,
+    callback: any
+  ) => (...args: any[]) => any;
 }
 const selectPath = (path: string[], state: any): any =>
   path.length === 0
     ? state
     : selectPath(path.slice(1), state[path[0]]);
+export const CREATE_SELECTOR = "CREATE_SELECTOR";
 const createBridge = ({
   getId = ({ id }: Id) => id,
   path = [],
   queryToString = (query) => JSON.stringify(query),
+  override = (_type, callback) => (...args) =>
+    callback(...args),
 }: Arg = {}) => {
-  const createSelectResult = (query: any) => (
-    state: any
-  ) => {
-    if (getId(query)) {
-      return (
-        selectPath(path, state).data[getId(query)] ||
-        NOT_REQUESTED
+  const createSelectResult = override(
+    CREATE_SELECTOR,
+    (query: any) => (state: any) => {
+      if (getId(query)) {
+        return (
+          selectPath(path, state).data[getId(query)] ||
+          NOT_REQUESTED
+        );
+      }
+      return mapResults(
+        selectPath(path, state).queries[
+          queryToString(query)
+        ] || NOT_REQUESTED
+      )((ids: number[] | string[]) =>
+        mapResults(
+          ...ids.map(
+            (id: any) => state.data[id] || NOT_REQUESTED
+          )
+        )((...items: any[]) => items)
       );
     }
-    return mapResults(
-      selectPath(path, state).queries[
-        queryToString(query)
-      ] || NOT_REQUESTED
-    )((ids: number[] | string[]) =>
-      mapResults(
-        ...ids.map(
-          (id: any) => state.data[id] || NOT_REQUESTED
-        )
-      )((...items: any[]) => items)
-    );
-  };
+  );
   return {
     createSelectResult,
   };
